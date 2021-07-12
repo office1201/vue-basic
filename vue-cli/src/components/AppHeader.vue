084919ad482ec2d1ccaa6bc802078f887f872652
iron0712 <office.div1201@gmail.com>
2021-07-12 23:14:31 +0900

    Vue-Basic  - Add: 1-1 기존 웹 개발 방식  - Add: 1-2 Reactivity 구현  - Modify: 1-2 Reactivity 추가 설명  - Add: 1-3 Reactivity 코드 라이브러리화  - Add: 1-4 Hello Vue.js와 개발자 도구 및 CDN 적용  - Add: 2-1 인스턴스 소개  - Add: 2-2 인스턴스와 생성자 함수 및 뷰를 생성자 함수로 찍어내는 이유  - Add: 2-3 인스턴스 옵션 속성 및 좋은 표기법  - Add: 3-1 컴포넌트 소개 및 전역 컴포넌트 등록 실습  - Modify: 제목 간격 수정  - Add: 3-2 지역 컴포넌트 등록 실습  - Modify: 제목 수정  - Add: 3-3 전역 컴포넌트와 지역 컴포넌트의 차이  - Add: 3-4 컴포넌트와 인스턴스의 관계  - Add: 4-2 프롭스 특징  - Add: 4-3 프롭스 실습  - Add: 4-4 이벤트 에밋과 이벤트 에밋을 통한 하위 컴포넌트와 상위 컴포넌트 간에 대화하는 방식  - Add: 같은 컴포넌트 레벨 간의 통신 방법  - Add: 5-1 뷰 라우터 설치  - Add: 5-2 뷰 라우터 인스턴스 연결 및 초기 상태  - Add: 라우터-뷰와 라우터-링크  - Add: 추후 학습 방향선 안내, 라우터-네이게이션 가드  - Add: 6 액시오스  - Add: 7-1 템플릿 문법, 데이터 바인딩과 computed 속성, 뷰 디렉티브와 v-bind, 클래스 바인딩과 v-if와 v-show  - Add: 7-2 methods 속성과 v-on 디렉티브를 이용한 키보드, 마우스 이벤트 처리 방법  - Add: 8-1 인스턴스의 왓치 속성  - Add: 8-2 인스턴스 속성, watch 속성과 computed 속성의 차이  - Add: 8-3 computed 속성을 이용한 클래스 코드 작성법(참 혹은 거짓 스타일링)  - Add: 요약 및 정리

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>header</h1>
+    <!-- 8. 싱글 파일 컴포넌트에서 이벤트 에밋 구현하기 -->
+    <!--  -클릭 이벤트 정의 -->
+    <button v-on:click="sendEvent">이벤트 보내기 버튼</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  methods: {
+    sendEvent: function () {
+      this.$emit("pass"); // -위쪽 컴포넌트로 이벤트 전달, $this.emit("보낼 이벤트의 이름"), 올렸을 때 props의 값을 바꿔볼 것
+    },
+  },
+};
+</script>
+
+<style>
+</style>
